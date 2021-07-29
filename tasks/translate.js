84ec0edda5ab2463cc3e0d6f10b8cbd01a2a3e2a
Cory Hughart <cory@blackbird.digital>
2021-07-28 23:39:29 -0400

    translate task

---
+++ tasks/translate.js
@@ -0,0 +1,56 @@
+/**
+ * Task: translate
+ */
+
+// Node
+const { join } = require( 'path' );
+const { promisify } = require( 'util' );
+
+// External
+const fileData = promisify( require( 'wp-get-file-data' ) );
+const gulp = require( 'gulp' );
+const sort = require( 'gulp-sort' );
+const wpPot = require( 'gulp-wp-pot' );
+
+// Internal
+const { handleStreamError, logEntries } = require( '../util' );
+
+module.exports = ( {
+	src = [ '**/*.php', '!node_modules/**/*', '!**/*.asset.php' ],
+	dest = 'languages_',
+} = {} ) => {
+	return function translate() {
+		// TODO: different file if plugin
+		const metadataFile = 'style.css';
+		return fileData( metadataFile, {
+			package: 'Theme Name',
+			domain: 'Text Domain',
+		} ).then( ( { domain, package } ) => {
+			// Add metadataFile to src array
+			if ( Array.isArray( src ) ) {
+				src.push( metadataFile );
+			} else {
+				src = [ src, metadataFile ];
+			}
+
+			return (
+				gulp
+					.src( src )
+					.pipe( handleStreamError( 'styles' ) )
+					.pipe( sort() )
+					//.pipe( logEntries( 'Translate files:' ) )
+					.pipe(
+						wpPot( {
+							domain,
+							package,
+							//bugReport: 'https://example.com',
+							//lastTranslator: 'Your Name Here <you@example.com>',
+							//team: 'Team Name Here <team@example.com>',
+							metadataFile,
+						} )
+					)
+					.pipe( gulp.dest( join( dest, `${ domain }.pot` ) ) )
+			);
+		} );
+	};
+};
