8d633c87a53395ad412c6e9c7708ab0285cb8a5c
Cory Hughart <cory@blackbird.digital>
2021-07-28 22:11:03 -0400

    scripts task

---
+++ tasks/scripts.js
@@ -0,0 +1,34 @@
+/**
+ * Task: scripts
+ */
+
+// External
+const gulp = require( 'gulp' );
+const destClean = require( 'gulp-dest-clean' );
+const named = require( 'vinyl-named' );
+const webpack = require( 'webpack' );
+const webpackStream = require( 'webpack-stream' );
+const wpWebpackConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+// Internal
+const { handleStreamError, logEntries } = require( '../util' );
+
+module.exports = ( { src = 'src/scripts/*.*', dest = 'dist/js' } = {} ) => {
+	return function scripts() {
+		const webpackConfig = {
+			...wpWebpackConfig,
+			devtool: 'source-map',
+		};
+		// Remove config props that may interfere with webpackStream
+		delete webpackConfig[ 'entry' ];
+
+		return gulp
+			.src( src, { sourcemaps: true } )
+			.pipe( handleStreamError( 'styles' ) )
+			.pipe( logEntries( 'Script entries:' ) )
+			.pipe( named() )
+			.pipe( webpackStream( webpackConfig, webpack ) )
+			.pipe( gulp.dest( dest, { sourcemaps: '.' } ) )
+			.pipe( destClean( dest ) );
+	};
+};
