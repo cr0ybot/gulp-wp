3dbc83fb5bc17a4f230cbbadd5e5ce8c75f080d9
Cory Hughart <cory@blackbird.digital>
2021-08-03 12:00:19 -0400

    warn about translate task issues

--- tasks/translate.js
+++ tasks/translate.js
@@ -13,52 +13,84 @@ const sort = require( 'gulp-sort' );
 const wpPot = require( 'gulp-wp-pot' );
 
 // Internal
-const { handleStreamError } = require( '../util' );
+const { c, handleStreamError, log } = require( '../util' );
 
 module.exports = {
 	task: ( gulp, { src, dest }, registry ) => {
 		return function translate() {
 			const { plugin } = registry.config;
-			const metadataFile = plugin || 'style.css';
-			const projectType = plugin ? 'Plugin' : 'Theme';
+			const metadataFile = plugin !== undefined ? plugin : 'style.css';
+			const projectType = plugin !== undefined ? 'Plugin' : 'Theme';
 
 			return fileData( metadataFile, {
 				package: `${ projectType } Name`,
 				domain: 'Text Domain',
-			} ).then( ( { domain, package } ) => {
-				// Add metadataFile to src array
-				if ( Array.isArray( src ) ) {
-					src.push( metadataFile );
-				} else {
-					src = [ src, metadataFile ];
-				}
-
-				return gulp
-					.src( src )
-					.pipe( handleStreamError( 'styles' ) )
-					.pipe( sort() )
-					.pipe(
-						wpPot( {
-							domain,
-							package,
-							//bugReport: 'https://example.com',
-							//lastTranslator: 'Your Name Here <you@example.com>',
-							//team: 'Team Name Here <team@example.com>',
-							metadataFile,
-						} )
-					)
-					.pipe(
-						gulp.dest(
-							join( dest, `${ domain || 'translations' }.pot` )
-						)
-					)
-					.pipe(
-						logFiles( {
-							title: 'translate result:',
-							showCount: false,
-						} )
+			} )
+				.catch( ( err ) => {
+					log.warn(
+						c.yellow(
+							'No metadata file found. Make sure you have a'
+						),
+						c.magenta( 'style.css' ),
+						c.yellow( 'file with' ),
+						c.cyan( 'Theme Name' ),
+						c.yellow(
+							'header field if your project is a theme or a main plugin file with the required'
+						),
+						c.cyan( 'Plugin Name' ),
+						c.yellow( 'header field.' )
 					);
-			} );
+					return {};
+				} )
+				.then( ( { domain, package } ) => {
+					// Add metadataFile to src array
+					if ( metadataFile ) {
+						if ( Array.isArray( src ) ) {
+							src.push( metadataFile );
+						} else {
+							src = [ src, metadataFile ];
+						}
+					}
+
+					if ( ! domain ) {
+						log.warn(
+							c.cyan( 'Text Domain' ),
+							c.yellow(
+								'not found, all text domains will be included.'
+							)
+						);
+					}
+
+					return gulp
+						.src( src )
+						.pipe( handleStreamError( 'styles' ) )
+						.pipe( sort() )
+						.pipe(
+							// TODO: wpPot config options
+							wpPot( {
+								domain,
+								package,
+								//bugReport: 'https://example.com',
+								//lastTranslator: 'Your Name Here <you@example.com>',
+								//team: 'Team Name Here <team@example.com>',
+								metadataFile,
+							} )
+						)
+						.pipe(
+							gulp.dest(
+								join(
+									dest,
+									`${ domain || 'translations' }.pot`
+								)
+							)
+						)
+						.pipe(
+							logFiles( {
+								title: `${ c.cyan( 'translate' ) } result:`,
+								showCount: false,
+							} )
+						);
+				} );
 		};
 	},
 	config: {
