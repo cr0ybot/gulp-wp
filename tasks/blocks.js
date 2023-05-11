445aab34680e0212ac629e21a459fae0f6c29c9f
Cory Hughart <cr0ybot@gmail.com>
2023-05-11 12:54:49 -0400

    clean up blocks task formatting

--- tasks/blocks.js
+++ tasks/blocks.js
@@ -21,15 +21,18 @@ const wpWebpackConfig = require( '@wordpress/scripts/config/webpack.config' );
 // GulpWP
 const { c, changed, handleStreamError, log, logFiles } = require( '../util' );
 
-const blockAssets = [ 'script', 'style', 'editorScript', 'editorStyle', 'viewScript' ];
+const blockAssets = [
+	'script',
+	'style',
+	'editorScript',
+	'editorStyle',
+	'viewScript',
+];
 
 module.exports = {
 	task: ( gulp, { src, entries, dest, includePaths } ) => {
-		return function blocks( done ) {
-			const filterOthers = filter( [
-				'*.{php,json}',
-				'!*.asset.php',
-			] );
+		return function blocks() {
+			const filterOthers = filter( [ '*.{php,json}', '!*.asset.php' ] );
 
 			/**
 			 * Overwrites pieces of the default webpack config.
@@ -70,7 +73,7 @@ module.exports = {
 								use: use.map( ( u ) => {
 									const { loader, options } = u;
 									if (
-										loader ==
+										loader ===
 										require.resolve( 'sass-loader' )
 									) {
 										return {
@@ -101,65 +104,83 @@ module.exports = {
 				},
 				plugins: [
 					new RemoveEmptyScriptsPlugin(),
-					...(( wpWebpackConfig?.plugins || [] ).filter(plugin => ! (plugin instanceof CopyWebpackPlugin))),
+					...( wpWebpackConfig?.plugins || [] ).filter(
+						( plugin ) => ! ( plugin instanceof CopyWebpackPlugin )
+					),
 				],
 				devtool: 'source-map',
 			};
 			// Remove config props that may interfere with webpackStream
-			delete webpackConfig[ 'entry' ];
+			delete webpackConfig.entry;
 
 			/**
 			 * Reads block.json files for block assets.
 			 *
 			 * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#assets
 			 */
-			const streamBlockAssets = through2.obj(function filterBlockAssetEntries( file, enc, cb ) {
-				log.debug(file.path);
-				// Parse block.json for asset files.
-				const data = JSON.parse( file.contents.toString() );
-				if ( data ) {
-					// Check each potential blockAsset prop.
-					for (const key of blockAssets) {
-						if ( data.hasOwnProperty( key ) ) {
-							// Value can be string or array, ensure array for consistency.
-							if ( ! Array.isArray( data[key] ) ) {
-								data[key] = [ data[key] ];
-							}
+			const streamBlockAssets = through2.obj(
+				function filterBlockAssetEntries( file, enc, cb ) {
+					log.debug( file.path );
+					// Parse block.json for asset files.
+					const data = JSON.parse( file.contents.toString() );
+					if ( data ) {
+						// Check each potential blockAsset prop.
+						for ( const key of blockAssets ) {
+							if ( data.hasOwnProperty( key ) ) {
+								// Value can be string or array, ensure array for consistency.
+								if ( ! Array.isArray( data[ key ] ) ) {
+									data[ key ] = [ data[ key ] ];
+								}
 
-							for ( const asset of data[key] ) {
-								if ( asset.startsWith('file:') ) {
-									const assetFile = asset.substring(5);
-									log.debug('block asset:', c.blue(assetFile));
-									let assetPath = dirname(file.path);
-									// If asset is js, glob for js, ts, jsx, or tsx.
-									if (assetFile.endsWith('js')) {
-										assetPath += `/${assetFile.slice(0, -2)}{j,t}{s,sx}`;
+								for ( const asset of data[ key ] ) {
+									if ( asset.startsWith( 'file:' ) ) {
+										const assetFile = asset.substring( 5 );
+										log.debug(
+											'block asset:',
+											c.blue( assetFile )
+										);
+										let assetPath = dirname( file.path );
+										// If asset is js, glob for js, ts, jsx, or tsx.
+										if ( assetFile.endsWith( 'js' ) ) {
+											assetPath += `/${ assetFile.slice(
+												0,
+												-2
+											) }{j,t}{s,sx}`;
+										}
+										// If asset is css, glob for sass, scss, or css.
+										else if (
+											assetFile.endsWith( 'css' )
+										) {
+											assetPath += `/${ assetFile.slice(
+												0,
+												-3
+											) }{sa,sc,c}ss`;
+										}
+										// If neither, just pass the file through, consequences be damned.
+										else {
+											assetPath += `/${ assetFile }`;
+										}
+										vinylRead
+											.sync( assetPath )
+											.map( ( f ) => this.push( f ) );
 									}
-									// If asset is css, glob for sass, scss, or css.
-									else if (assetFile.endsWith('css')) {
-										assetPath += `/${assetFile.slice(0, -3)}{sa,sc,c}ss`;
-									}
-									// If neither, just pass the file through, consequences be damned.
-									else {
-										assetPath += `/${assetFile}`;
-									}
-									vinylRead.sync(assetPath).map(f => this.push(f));
 								}
 							}
 						}
+
+						// End processing and remove this file from the stream.
+						return cb();
 					}
 
-					// End processing and remove this file from the stream.
-					return cb();
+					// Pass the file through.
+					return cb( null, file );
 				}
-
-				// Pass the file through.
-				return cb( null, file );
-			});
+			);
 
 			return merge(
 				// Handle block assets.
-				gulp.src( entries )
+				gulp
+					.src( entries )
 					.pipe( handleStreamError( 'blocks' ) )
 					// Filter out all but entrypoints.
 					.pipe( streamBlockAssets )
@@ -178,7 +199,8 @@ module.exports = {
 					.pipe( webpackStream( webpackConfig, webpack ) )
 					.pipe( gulp.dest( dest ) ),
 				// Handle moving other files (php, json)
-				gulp.src( src )
+				gulp
+					.src( src )
 					.pipe( handleStreamError( 'blocks' ) )
 					.pipe( filterOthers )
 					.pipe( changed( gulp.lastRun( blocks ) ) )
